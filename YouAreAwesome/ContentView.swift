c5966f13b3db17db5a012111641335190616c8b5
schmidln <schmidln@bc.edu>
2025-01-20 19:26:56 -0500

    Capitalized "I Am A Programmer!"

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var message = "I am a programmer!"
+    @State private var message = "I Am A Programmer!"
     var body: some View {
         VStack {
             Image(systemName: "swift")
