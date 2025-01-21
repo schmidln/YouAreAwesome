2f125c5f3db6f7ca889013e53d9fd2833d60af64
schmidln <schmidln@bc.edu>
2025-01-20 22:21:09 -0500

    Ch 1.3

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -11,17 +11,29 @@ struct ContentView: View {
     @State private var message = "I Am A Programmer!"
     var body: some View {
         VStack {
+            Spacer()
             Image(systemName: "swift")
                 .resizable()
                 .scaledToFit()
                 .foregroundColor(.orange)
+                .frame(width: 200, height: 200)
             Text(message)
                 .font(.largeTitle)
-                .fontWeight(.heavy)
-                .foregroundStyle(.red)
-            Button("Click Me!") {
-                message = "Awesome!"
+                .fontWeight(.ultraLight)
+            Spacer()
+            HStack {
+                Button("Awesome") {
+                    message = "Awesome!"
+                }
+                Button("Great", action: {
+                    message = "Great!"
+                })
             }
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
+            .tint(.orange)
+            
+            
         }
         .padding()
     }
